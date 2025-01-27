2e7f395ed8982b172c7dbd28b74f5154a6031e08
glorycode02 <glorycode02@gmail.com>
2025-01-27 08:48:59 +0200

    done overflow

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,64 +30,66 @@ class HomePage extends StatelessWidget {
         ),
       ),
       drawer: const MyDrawer(),
-      body: Column(
-        children: [
-          Center(
-            child: Container(
-              width: 400,
-              height: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.asset(
-                  "assets/images/image1.jpg",
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                width: 400,
+                height: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    "assets/images/image1.jpg",
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Column(
-              children: [
-                Center(
-                  child: Text(
-                    "Welcome to Pizza! 🍕, get your favourite pizza delivery on our Web!!!",
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  Center(
+                    child: Text(
+                      "Welcome to Pizza! 🍕, get your favourite pizza delivery on our Web!!!",
+                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Text(
-                  "Your perfect delivery on time is our mission🔥🔥🔥🔥,Lets work together to get onto our perfect company!!!",
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text.rich(
-                  TextSpan(
-                    text: "Top ",
-                    style: TextStyle(fontSize: 30),
-                    children: [
-                      TextSpan(
-                          text: "Products",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.green))
-                    ],
+                  Text(
+                    "Your perfect delivery on time is our mission🔥🔥🔥🔥,Lets work together to get onto our perfect company!!!",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
                   ),
-                ),
-                // SizedBox(height: 50,),
-                ProductCard(productimage: "assets/images/image2.jpg")
-              ],
-            ),
-          )
-        ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      text: "Top ",
+                      style: TextStyle(fontSize: 30),
+                      children: [
+                        TextSpan(
+                            text: "Products",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Colors.green))
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 50,),
+                  ProductCard(productimage: "assets/images/image2.jpg")
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
